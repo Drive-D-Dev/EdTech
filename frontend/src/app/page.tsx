10fa13a92a7a87dae3d35468d4b9a395e8145321
mels <chanatan.melmel@gmail.com>
2024-08-12 17:24:06 +0700

    feat: navbar SignInWrapper and initial styling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main className="container flex justify-center h-[60dvh] flex-col">
+    <main className="container flex justify-center flex-col">
       <div className="box flex space-x-2">
         <p className="">this is a box section</p>
       </div>
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,12 +1,57 @@
 import React from "react";
+import { LOGO_IMG, NAV_LINKS } from "@/constant/global";
+import Link from 'next/link'
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "./ui/button";
+
+
+// TODO: check whether or not user is sign in.
+const session = false
 
 const Navbar = () => {
   return (
-    <nav className="container bg-background/40 backdrop-blur-sm py-4 sticky w-full top-0 left-0 z-10">
-      <ModeToggle />
+    <nav className="container flex justify-between items-center bg-background/20 backdrop-blur-md py-4 sticky w-full top-2 left-2 z-10 rounded-full shadow-sm border">
+      <Link href="/">
+        <img src={LOGO_IMG.src} alt={LOGO_IMG.alt} height={64} width={64} />
+      </Link>
+
+      <div className="flex justify-center items-center gap-4">
+        {
+          NAV_LINKS.map((link) => (
+            <Link href={link.href}>
+              <Button variant="ghost">
+                {link.title}
+              </Button>
+            </Link>
+          ))
+        }
+        {
+          session ?
+            <ProfileWrapper /> :
+            <SignInWrapper />
+        }
+      </div>
     </nav>
   );
 };
+
+const SignInWrapper = () => {
+  return (
+    <>
+      <Button>Sign In</Button>
+      <ModeToggle />
+    </>
+  )
+}
+
+const ProfileWrapper = () => {
+  return (
+    <>
+      <Avatar>
+      </Avatar>
+    </>
+  )
+}
 
 export default Navbar;
