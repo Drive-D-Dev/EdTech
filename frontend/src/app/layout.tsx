b7e5e46d05172f0272d1c5b6759c6a2030049df0
mels <chanatan.melmel@gmail.com>
2024-08-13 00:46:44 +0700

    feat: style answer-dialog

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <Navbar />
             {children}
           </ThemeProvider>
-
         </div>
         <Toaster />
       </body>
--- frontend/src/components/answer-dialog.tsx
+++ frontend/src/components/answer-dialog.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
+import { MdOutlineLightbulb } from "react-icons/md";
+
 import { Button } from "@/components/ui/button"
 import {
     Dialog,
@@ -24,27 +25,38 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import useIsMobile from "@/hooks/use-is-mobile"
 
+const answerContent = {
+    question: "How is the weather today?",
+    description: "the weather is great"
+}
 export function AnswerDialog() {
     const [open, setOpen] = React.useState(false)
     const isMobile = useIsMobile()
+
+    const Trigger = () => (
+        <Button variant="outline" className="w-10 p-0 aspect-square">
+            <MdOutlineLightbulb className="text-xl" />
+        </Button>
+
+    )
 
     if (isMobile) {
         return (
             <Drawer open={open} onOpenChange={setOpen}>
                 <DrawerTrigger asChild>
-                    <Button variant="outline">Edit Profile</Button>
+                    <span>
+                        <Trigger />
+                    </span>
                 </DrawerTrigger>
                 <DrawerContent>
                     <DrawerHeader className="text-left">
-                        <DrawerTitle>Edit profile</DrawerTitle>
-                        <DrawerDescription>
-                            Make changes to your profile here. Click save when you're done.
-                        </DrawerDescription>
+                        <DrawerTitle>{answerContent.question}</DrawerTitle>
+
                     </DrawerHeader>
-                    <ProfileForm className="px-4" />
+                    <AnswerContent className="px-4" />
                     <DrawerFooter className="pt-2">
                         <DrawerClose asChild>
-                            <Button variant="outline">Cancel</Button>
+                            <Button variant="outline">Close</Button>
                         </DrawerClose>
                     </DrawerFooter>
                 </DrawerContent>
@@ -55,34 +67,25 @@ export function AnswerDialog() {
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
-                <Button variant="outline">Edit Profile</Button>
+                <span>
+                    <Trigger />
+                </span>
             </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
+            <DialogContent className="">
                 <DialogHeader>
-                    <DialogTitle>Edit profile</DialogTitle>
-                    <DialogDescription>
-                        Make changes to your profile here. Click save when you're done.
-                    </DialogDescription>
+                    <DialogTitle>{answerContent.question}</DialogTitle>
                 </DialogHeader>
-                <ProfileForm />
+                <AnswerContent />
             </DialogContent>
         </Dialog>
     )
 
 }
 
-function ProfileForm({ className }: React.ComponentProps<"form">) {
+function AnswerContent({ className }: React.ComponentProps<"div">) {
     return (
-        <form className={cn("grid items-start gap-4", className)}>
-            <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input type="email" id="email" defaultValue="shadcn@example.com" />
-            </div>
-            <div className="grid gap-2">
-                <Label htmlFor="username">Username</Label>
-                <Input id="username" defaultValue="@shadcn" />
-            </div>
-            <Button type="submit">Save changes</Button>
-        </form>
+        <div className={cn(className)}>
+            {answerContent.description}
+        </div>
     )
 }
