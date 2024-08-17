8d8d63f137a673efe94b1d3bc1adaf92aea16762
Phatteronyyz <67008753+Phatteronyyz@users.noreply.github.com>
2024-08-17 14:53:13 +0700

    added mock-page and answer-page

---
+++ frontend/src/components/answerbox.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface AnswerBoxProps {
+	label: string;
+	boxColor: string;
+}
+
+const AnswerBox: React.FC<AnswerBoxProps> = ({ label, boxColor }) => {
+	return (
+		<div className='flex justify-between items-center w-full h-fit p-4 border rounded-md'>
+			<div className='flex items-center space-x-5'>
+				<div className={`w-6 h-6 rounded-md ${boxColor}`} />
+				<p className='text-black text-md font-normal'>{label}</p>
+			</div>
+		</div>
+	);
+};
+
+export default AnswerBox;
