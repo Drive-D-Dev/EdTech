2976137d3c90df583c67a8d17188e0d57888ea44
Sunthewhat <chotiwet.wisi@icloud.com>
2024-09-12 15:00:28 +0700

    forget this one

--- server-core/src/routes/index.ts
+++ server-core/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono';
 import { exampleRouter } from './example.routes';
 import { questionRouter } from './question.routes';
+import { studySetRouter } from './studySet.routes';
 
 const mainRouter = new Hono();
 
 mainRouter.route('/example', exampleRouter);
 mainRouter.route('/question', questionRouter);
+mainRouter.route('/set', studySetRouter);
 
 export { mainRouter };
