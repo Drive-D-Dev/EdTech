941dd66ae275d1aac2da4a5179c32e134ac15a1a
Sunthewhat <chotiwet.wisi@icloud.com>
2024-09-17 20:49:31 +0700

    fix missing key props

--- frontend/src/components/navbar-menu.tsx
+++ frontend/src/components/navbar-menu.tsx
@@ -1,70 +1,57 @@
-import React from 'react'
-import { NAV_LINKS } from '@/constant/global'
-import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import React from 'react';
+import { NAV_LINKS } from '@/constant/global';
+import { HiOutlineMenuAlt3 } from 'react-icons/hi';
 
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-} from "@/components/ui/sheet"
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from '@/components/ui/sheet';
 import useIsMobile from '@/hooks/use-is-mobile';
 
-
 export const NavbarMenu = () => {
-    const isMobile = useIsMobile()
+	const isMobile = useIsMobile();
 
-    if (isMobile) {
-        return <MobileMenu />
-    }
+	if (isMobile) {
+		return <MobileMenu />;
+	}
 
-    return (
-        <>
-            {
-                NAV_LINKS.map((link) => (
-                    <Link href={link.href}>
-                        <Button variant="ghost">
-                            {link.title}
-                        </Button>
-                    </Link>
-                ))
-            }
-        </>
-    )
-}
+	return (
+		<>
+			{NAV_LINKS.map((link, index: number) => (
+				<Link href={link.href} key={index}>
+					<Button variant='ghost'>{link.title}</Button>
+				</Link>
+			))}
+		</>
+	);
+};
 
 const MobileMenu = () => {
-    return (
-        <Sheet>
-            <SheetTrigger>
-                <HiOutlineMenuAlt3 className='text-2xl' />
-            </SheetTrigger>
-            <SheetContent side="right">
-                <SheetHeader>
-                    <SheetTitle>
-                        Menu
-                    </SheetTitle>
-                </SheetHeader>
-                <div className="mt-10">
-                    {
-                        NAV_LINKS.map((link) => (
-                            <Link href={link.href}>
-                                <Button
-                                    className='w-full'
-                                    size="lg"
-                                    variant="ghost">
-                                    {link.title}
-                                </Button>
-                            </Link>
-                        ))
-                    }
-                </div>
-            </SheetContent>
-        </Sheet>
-
-    )
-}
+	return (
+		<Sheet>
+			<SheetTrigger>
+				<HiOutlineMenuAlt3 className='text-2xl' />
+			</SheetTrigger>
+			<SheetContent side='right'>
+				<SheetHeader>
+					<SheetTitle>Menu</SheetTitle>
+				</SheetHeader>
+				<div className='mt-10'>
+					{NAV_LINKS.map((link, index: number) => (
+						<Link href={link.href} key={index}>
+							<Button className='w-full' size='lg' variant='ghost'>
+								{link.title}
+							</Button>
+						</Link>
+					))}
+				</div>
+			</SheetContent>
+		</Sheet>
+	);
+};
