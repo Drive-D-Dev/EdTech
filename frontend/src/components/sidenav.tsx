5337b23b4fa892a28603ff9812ef10430323488e
mels <chanatan.melmel@gmail.com>
2024-09-18 11:28:45 +0700

    feat: responsive on layout

--- frontend/src/components/sidenav.tsx
+++ frontend/src/components/sidenav.tsx
@@ -1,42 +1,37 @@
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { PanelsLeftBottom, Star } from "lucide-react";
 import { LOGO_IMG } from "@/constant/global";
+
 import Link from "next/link";
 import Logo from "@/components/logo"
+import useIsMobile from '@/hooks/use-is-mobile';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { NavbarMenu } from '@/components/navbar-menu'
 
 const SideNav = () => {
-
-  const customButtonStyle = "w-full flex justify-start items-center gap-2";
   return (
-    <div className="flex flex-col h-full gap-5 justify-start items-start py-6">
+    <div className="flex sm:flex-col flex-row h-full gap-5 sm:justify-start   justify-between items-center sm:items-start sm:py-6 py-2">
       <Link href="/">
         <Logo />
       </Link>
-      <ul className="w-full mt-5">
-        <li>
-          <Button variant="ghost" size="lg" className={customButtonStyle} asChild>
-            <Link href={"/"}>
-              <PanelsLeftBottom size={16} /> หน้าหลัก
-            </Link>
-          </Button>
-        </li>
-        <li>
-          <Button variant="ghost" size="lg" className={customButtonStyle} asChild>
-            <Link href={"/upcomming"}>
-              <Star size={16} />
-              อีเว้น
-            </Link>
-          </Button>
-        </li>
+      <ul className="sm:w-full sm:mt-5">
+        <NavbarMenu />
       </ul>
-      <footer className="mt-auto flex flex-col items-start">
+      <footer className="mt-auto flex flex-col items-start sm:block hidden">
         <p className="text-sm text-muted-foreground">Copyright © 2024</p>
         <Button variant="link">เงื่อนไขของการบริการ</Button>
       </footer>
     </div>
   );
 };
+
 
 export default SideNav;
---
+++ frontend/src/constant/global.tsx
@@ -0,0 +1,27 @@
+import { PanelsLeftBottom, Star, Menu } from "lucide-react";
+
+export const LOGO_IMG = {
+  src: "/testmate-logo.svg",
+  alt: "logo",
+};
+
+interface Link {
+  title: string;
+  href: string;
+  icon: () => {};
+}
+export const NAV_LINKS = [
+  {
+    title: "หน้าหลัก",
+    href: "/",
+    icon: <PanelsLeftBottom size={16} />
+
+  },
+  {
+    title: "อีเว้น",
+    href: "/event",
+    icon: <Star size={16} />
+  },
+];
+
+export const BACKEND_URL = "http://localhost:8000";
