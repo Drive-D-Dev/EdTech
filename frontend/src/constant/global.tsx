1532a03f9ee719d23653d7e9ad10e2aa5bf9a474
Sunthewhat <chotiwet.wisi@icloud.com>
2024-09-20 01:39:47 +0700

    using .env file

--- frontend/src/constant/global.tsx
+++ frontend/src/constant/global.tsx
@@ -1,27 +1,26 @@
-import { PanelsLeftBottom, Star, Menu } from "lucide-react";
+import { PanelsLeftBottom, Star, Menu } from 'lucide-react';
 
 export const LOGO_IMG = {
-  src: "/testmate-logo.svg",
-  alt: "logo",
+	src: '/testmate-logo.svg',
+	alt: 'logo',
 };
 
 interface Link {
-  title: string;
-  href: string;
-  icon: () => {};
+	title: string;
+	href: string;
+	icon: () => {};
 }
 export const NAV_LINKS = [
-  {
-    title: "หน้าหลัก",
-    href: "/",
-    icon: <PanelsLeftBottom size={16} />
-
-  },
-  {
-    title: "อีเว้น",
-    href: "/event",
-    icon: <Star size={16} />
-  },
+	{
+		title: 'หน้าหลัก',
+		href: '/',
+		icon: <PanelsLeftBottom size={16} />,
+	},
+	{
+		title: 'อีเว้น',
+		href: '/event',
+		icon: <Star size={16} />,
+	},
 ];
 
-export const BACKEND_URL = "http://localhost:8000";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_AXIOSBASEURL || 'http://localhost:8000';
--- frontend/src/lib/axiosConfig.ts
+++ frontend/src/lib/axiosConfig.ts
@@ -1,8 +1,7 @@
-import { BACKEND_URL } from '@/constant/global';
 import axios, { AxiosInstance } from 'axios';
 
 const Axios: AxiosInstance = axios.create({
-	baseURL: BACKEND_URL || '',
+	baseURL: process.env.NEXT_PUBLIC_AXIOSBASEURL || '',
 	withCredentials: true,
 	validateStatus: (status) => status >= 200 && status < 500,
 });
